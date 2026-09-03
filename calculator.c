8732f4e03182992b4435cf2b455f1b22bbea82e1
chrisrandla <chrisrandla@gmail.com>
2026-09-03 11:24:00 -0500

    implemented division feature and added labels
    Added divide function and fixed function prototypes.

--- calculator.c
+++ calculator.c
@@ -1,12 +1,15 @@
+// header files and function prototypes
 #include <stdio.h>\
 float add(float x, float y);
 float subtract(float x, float y);
 float multiply(float x, float y);
-
+float divide(float x, float y);
+// Main function / user interface 
  main(){
 
 	 
  }
+// calculator functions
 float add(float x, float y) {
 	int ans = x + y;
 	return ans;
@@ -17,5 +20,9 @@ float subtract(float x, float y); {
 }
 float multiply(float x, float y); {
 	int ans = x * y;
+	return ans;
+}
+float divide(float x, float y); {
+	int ans = x / y;
 	return ans;
 }
