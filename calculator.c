7a5694f77c6594f11b7b357b4ed51d93e34e08aa
chrisrandla <chrisrandla@gmail.com>
2026-09-03 11:30:04 -0500

    added comments and documentation
    Added error handling for division by zero and invalid operators.

--- calculator.c
+++ calculator.c
@@ -8,13 +8,15 @@ float divide(float x, float y);
  main() {
 float x, y;
 	char operator;
+//take user inputs for the calculator 
 	printf("Enter first number: ");
 	scanf("%f", &x);
+// tell the calculator what function to use 
 	printf("Enter operator (+, -, *, /): ");
 	scanf(" %c", &operator);
 	printf("Enter second number: ");
 	scanf("%f", &y);
-
+// use the char to determine what function were using and then return the value 
 	switch (operator) {
 	case '+':
 		printf("%.2f + %.2f = %.2f\n", x, y, add(x, y));
@@ -26,11 +28,14 @@ float x, y;
 		printf("%.2f * %.2f = %.2f\n", x, y, multiply(x, y));
 		break;
 	case '/':
+		//check for divide by zero then divide 
 		if (y != 0) {
 			printf("%.2f / %.2f = %.2f\n", x, y, divide(x, y));
 		}
+			// else statement for if y == 0 
 		else {
 			printf("Error: Division by zero is not allowed.\n");
+	// make sure that the user gave a valid operator		
 	default:
 		printf("Error: Invalid operator.\n");
 	}
