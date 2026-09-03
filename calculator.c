022ad794355bdfdf0d1567d42aa4ec2c9d25f95c
chrisrandla <chrisrandla@gmail.com>
2026-09-03 11:21:27 -0500

    Implemented subtract feature

--- calculator.c
+++ calculator.c
@@ -1,9 +1,14 @@
 #include <stdio.h>\
 float add(float x, float y);
+float subtract(float x, float y);
 
  main(){
  }
 float add(float x, float y) {
 	int ans = x + y;
 	return ans;
+}
+float subtract(float x, float y); {
+	int ans = x - y;
+	return ans; 
 }
